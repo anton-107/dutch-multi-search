e99f48a6aaa093f4f79e3acff876615b0d3e850e
Anton Nekipelov <anton.nekipelov@gmail.com>
2016-04-10 10:23:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Multi-search for Dutch language
+Usage:
+1. Open src/index.html page
+2. Type in a word you want to search
+3. Hit enter
+4. Multiple tabs will be open in your browser to show your word in different searches
+
+## Search results supported
+1. Google Images
+2. forvo (pronounciations)
+3. VanDale (nl-nl)
+4. Wiktionary (nl in english)
+5. verbix.com (verb forms, only if the search ends with "en")
+6. interglot (uncomment in main.js to enable)
+7. bab.la (english translation) (uncomment in main.js to enable)
+
+## Features
+1. Opens multiple tabs for one word search (if your browser blocks pop-ups, enable them for this page)
+2. Closes tabs with previous searches automatically on new search
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+var form = document.getElementsByClassName("search-form")[0];
+var input = document.getElementsByClassName("search-form__text")[0];
+var activeWindows = [];
+
+document.getElementsByClassName("search-form")[0].addEventListener("submit", function (e) {
+  e.preventDefault();
+  var q = input.value;
+  closeActiveWindows();
+  if (q.length > 0) {
+    console.log('test', q);
+    activeWindows.push(window.open("https://www.google.nl/search?tbm=isch&q=" + q));
+    activeWindows.push(window.open("http://forvo.com/word/" + q + "/#nl"));
+    activeWindows.push(window.open("https://en.wiktionary.org/wiki/" + q + "#Dutch"));
+
+    if (q.length > 2 && q.substr(-2) === "en") {
+      activeWindows.push(window.open("http://www.verbix.com/webverbix/go.php?T1=" + q + "&Submit=Go&D1=24&H1=124"));
+    }
+
+    activeWindows.push(window.open("http://www.vandale.nl/opzoeken?pattern=" + q + "&lang=nn"));
+    // activeWindows.push(window.open("http://www.interglot.com/dictionary/nl/en/search?q=" + q));
+    // activeWindows.push(window.open("http://en.bab.la/dictionary/dutch-english/" + q));
+  }
+});
+
+function closeActiveWindows() {
+  activeWindows.forEach(function (w) {
+    w.close();
+  });
+  activeWindows = [];
+}
